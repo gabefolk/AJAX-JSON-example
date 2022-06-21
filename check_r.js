dc2de65eea0200b7986f9141bf8aed859e4561de
gabefolk <98960465+gabefolk@users.noreply.github.com>
2022-06-21 01:46:58 -0600

    Add files via upload

---
+++ check_r.js
@@ -0,0 +1,4 @@
+document.getElementById("country").addEventListener("input", getCountries, false); // event registration for AJAX
+document.getElementById("city").addEventListener("input", getCities, false); 
+document.getElementById("countrySubmit").addEventListener("click", getCity, false); 
+document.getElementById("citySubmit").addEventListener("click", getCountry, false); 
